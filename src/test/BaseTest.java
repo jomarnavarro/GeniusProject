1eebf6a430ef7fcd86cbb4813ad390bfbd70a904
Omar Navarro <jesusbalderas@cloud.upwork.com>
2019-07-11 20:52:01 -0500

    Implements the methodo startBrowser

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -1,13 +1,18 @@
 package test;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 	
 	static WebDriver driver;
 	
-	protected static void arrancarBrowser(String string) {
-		// TODO Auto-generated method stub
+	protected static void arrancarBrowser(String url) {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get(url);
 		
 	}
 
